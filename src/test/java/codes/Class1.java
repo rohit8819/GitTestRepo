ea13d0fadcd7b8ee09ab328b58da553dd1b4b996
rohit2 <rohit2@user.com>
2024-01-31 00:20:54 +0530

    changes by user1

--- src/test/java/codes/Class1.java
+++ src/test/java/codes/Class1.java
@@ -7,7 +7,7 @@ public class Class1 {
 @Test
  public void sampleMethod(){
 
- System.out.println("first method");
+ System.out.println("first method - changes by user1");
  System.out.println("changes by user 2");
 
  System.out.println("code for undo by user 2");
