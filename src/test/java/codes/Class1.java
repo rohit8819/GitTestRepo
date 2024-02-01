b6a7c9bb1fd82d13837d43b937345b7ac902d99b
rohit2 <rohit2@user.com>
2024-02-01 23:07:13 +0530

    deleted old code

--- src/test/java/codes/Class1.java
+++ src/test/java/codes/Class1.java
@@ -5,18 +5,9 @@ import org.testng.annotations.Test;
 public class Class1 {
 
 @Test
- public void sampleMethod(){
+ public void class1Method(){
 
 
-
- System.out.println("first method -- changes by user1");
- System.out.println("changes by user 2 for login feature");
- System.out.println("test1 + changes by user1");
- System.out.println("test2");
- System.out.println("test3");
- System.out.println("code for undo by user 2");
 
  }
-
-
 }
---
+++ src/test/java/codes/Class2.java
@@ -0,0 +1,13 @@
+package codes;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+
+    @Test
+    public void class2Method(){
+
+
+
+    }
+}
